8d11c3ab169efd9a08f267850f787ab8b0b7b1cb
andorgast <andorgast@gmail.com>
2025-11-18 15:51:28 +0100

    Basic layout done.

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,20 @@
+<script setup lang="ts">
+let Temperature = "12"
+</script>
 <template>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <div class="m-[100px] flex justify-between">
+    <div>
+      <p class="bold text-6xl">Data:</p>
+      <p class="text-4xl">Temperature:</p>
+      <p class="text-4xl">Raining:</p>
+      <p class="text-4xl">Humidity:</p>
+      <p class="text-4xl">Air pressure:</p>
+      <p class="text-4xl">Air quality:</p>
+      <p class="text-4xl">Time:</p>
+    </div>
+    <div>
+      <img src="/media/png-transparent-rain-rain-blue-cloud-drop-thumbnail.png">
+    </div>
+  </div>
 </template>
