739ac0b4b2c00896d4cd112fba45f7397db61b9c
andorgast <andorgast@gmail.com>
2025-11-19 16:18:06 +0100

    Graph is done.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,63 @@
-<script setup lang="ts">
-let Temperature = "12"
+<script setup language="ts">
+// import('highcharts/highcharts').Options
+const data = [[1763506800000, 20],[new Date().getTime(),21]];
+
+const temperatureGraph = ref({
+  chart: {
+    zooming: {
+      type: 'x'
+    }
+  },
+  title: {
+    text: 'AAAA I HATE MY LIFE'
+  },
+  subtitle: {
+    text: 'Pinch the chart to zoom in'
+  },
+  xAxis: {
+    type: 'datetime',
+  },
+  yAxis: {
+    title: {
+      text: 'Graden Celcius'
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  plotOptions: {
+    area: {
+      marker: {
+        radius: 2
+      },
+      lineWidth: 1,
+      color: {
+        linearGradient: {
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 1
+        },
+        stops: [
+          [0, 'rgb(199, 113, 243)'],
+          [0.7, 'rgb(76, 175, 254)']
+        ]
+      },
+      states: {
+        hover: {
+          lineWidth: 5
+        }
+      },
+      threshold: null
+    }
+  },
+
+  series: [{
+    type: 'area',
+    name: 'Graden Celcius',
+    data: data
+  }]
+})
 </script>
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,4 +75,8 @@ let Temperature = "12"
       <img src="/media/png-transparent-rain-rain-blue-cloud-drop-thumbnail.png">
     </div>
   </div>
+  <highchart
+      :options="temperatureGraph"
+      :update="['options.title', 'options.series']"
+  />
 </template>
