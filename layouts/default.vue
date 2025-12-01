5132d82f7e2c47ccc0f878e2b6bbc0d7483195eb
andorgast <andorgast@gmail.com>
2025-12-01 16:00:11 +0100

    removed unnecessary components in the template.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-
-import FooterComponent from "~/components/footerComponent.vue";
 </script>
-
-<!--<template>-->
-<!--  <header-component/>-->
-<!--  <NuxtPage/>-->
-<!--  <footer-component/>-->
-<!--</template>-->
 <template>
     <div class="min-h-screen grid grid-rows-[auto_1fr_auto]">
       <header>
